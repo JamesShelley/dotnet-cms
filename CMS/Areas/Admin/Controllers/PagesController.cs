729704aada15f747958bf639bb928d265028521f
James Shelley <jcs@tuta.io>
2018-06-20 05:52:48 +0100

    Created the pages database and display table data.

---
+++ CMS/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CMS.Models.Data;
+using CMS.Models.ViewModels.Pages;
+
+namespace CMS.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            // Declare a list of PageVM
+            List<PageVM> pagesList;
+
+            using (Db db = new Db())
+            {
+                //Init the list
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+            //Return the view with the list
+            return View(pagesList);
+        }
+    }
+}
